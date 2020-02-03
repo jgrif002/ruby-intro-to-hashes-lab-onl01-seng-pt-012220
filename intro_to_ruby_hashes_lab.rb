06e79a7d7a2e35890eab730d95a65ac3383596e2
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-03 05:44:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -63,11 +63,11 @@ def monopoly_with_fourth_tier
 	      },
 	      :b_and_o_railroad => 
 	      {
-	        "mortgage_value" => "$300"
+	        "mortgage_value" => "$400"
 	      },
 	      :shortline => 
 	      {
-	        "mortgage_value" => "$400"
+	        "mortgage_value" => "$800"
 	      }
 	    },
 	    :rent_in_dollars => 
